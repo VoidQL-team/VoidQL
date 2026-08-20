7bb9c0fad53f1a9486ab0fcd0149aef5fe5a43e2
Zucchy00 <zucchelli.dani@gmail.com>
2026-08-20 17:26:42 +0000

    updated package

--- tests/auto.test.ts
+++ tests/auto.test.ts
@@ -102,7 +102,7 @@ async function runTests(mode: "sequential" | "parallel" = "sequential") {
   return Promise.all(testFiles.map(runOne));
 }
 
-it("runs OrbitQL auto test suite", async () => {
+it("runs VoidQL auto test suite", async () => {
   const results = await runTests(
     run_mode === "parallel" ? "parallel" : "sequential"
   );
